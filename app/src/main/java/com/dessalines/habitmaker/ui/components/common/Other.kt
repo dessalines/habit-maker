34d8a860adc494ef315b16f600f1d0cf15c3168b
Dessalines <tyhou13@gmx.com>
2025-05-14 18:39:15 -0400

    Adding android lint.

--- app/src/main/java/com/dessalines/habitmaker/ui/components/common/Other.kt
+++ app/src/main/java/com/dessalines/habitmaker/ui/components/common/Other.kt
@@ -50,12 +50,13 @@ fun TodayCompletedCount(todayCompletedCount: Int) =
     )
 
 @Composable
-fun Modifier.textFieldBorder() =
-    Modifier.border(
+fun Modifier.textFieldBorder(): Modifier {
+    return this then Modifier.border(
         width = OutlinedTextFieldDefaults.UnfocusedBorderThickness,
         color = OutlinedTextFieldDefaults.colors().unfocusedIndicatorColor,
         shape = OutlinedTextFieldDefaults.shape,
     )
+}
 
 @Composable
 fun HabitInfoChip(
