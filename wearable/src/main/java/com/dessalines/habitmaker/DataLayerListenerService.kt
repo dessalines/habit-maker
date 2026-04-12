0d76b187a0242d318fc701aaa0a8395ba928956d
Dessalines <tyhou13@gmx.com>
2026-04-12 09:21:31 -0400

    Fixing unused imports

--- wearable/src/main/java/com/dessalines/habitmaker/DataLayerListenerService.kt
+++ wearable/src/main/java/com/dessalines/habitmaker/DataLayerListenerService.kt
@@ -1,23 +1,15 @@
 package com.dessalines.habitmaker
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.util.Log
-import com.dessalines.habitmaker.presentation.MainActivity
 import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.DataMapItem
-import com.google.android.gms.wearable.MessageEvent
-import com.google.android.gms.wearable.Node
-import com.google.android.gms.wearable.Wearable
 import com.google.android.gms.wearable.WearableListenerService
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import utils.TAG
+import com.dessalines.habitmaker.utils.TAG
 
 class DataLayerListenerService : WearableListenerService() {
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
