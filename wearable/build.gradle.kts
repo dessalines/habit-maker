585bb3f946cff5ef3718c20842f8835732ace543
Dessalines <tyhou13@gmx.com>
2026-04-13 11:55:27 -0400

    Signing for wear app

--- wearable/build.gradle.kts
+++ wearable/build.gradle.kts
@@ -22,8 +22,35 @@ android {
         ksp { arg("room.schemaLocation", "$projectDir/schemas") }
     }
 
+    // Necessary for izzyondroid releases
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
+
+    if (project.hasProperty("RELEASE_STORE_FILE")) {
+        signingConfigs {
+            create("release") {
+                storeFile = file(project.property("RELEASE_STORE_FILE")!!)
+                storePassword = project.property("RELEASE_STORE_PASSWORD") as String?
+                keyAlias = project.property("RELEASE_KEY_ALIAS") as String?
+                keyPassword = project.property("RELEASE_KEY_PASSWORD") as String?
+
+                // Optional, specify signing versions used
+                enableV1Signing = true
+                enableV2Signing = true
+            }
+        }
+    }
+
+
     buildTypes {
         release {
+            if (project.hasProperty("RELEASE_STORE_FILE")) {
+                signingConfig = signingConfigs.getByName("release")
+            }
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
