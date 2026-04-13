565a0a5bc644afa1d2077bb61746852b6b6efc42
Dessalines <tyhou13@gmx.com>
2026-04-13 10:42:50 -0400

    Adding complication

--- wearable/src/main/java/com/dessalines/habitmaker/complication/MainComplicationService.kt
+++ wearable/src/main/java/com/dessalines/habitmaker/complication/MainComplicationService.kt
@@ -1,42 +1,84 @@
 package com.dessalines.habitmaker.complication
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationType
 import androidx.wear.watchface.complications.data.PlainComplicationText
 import androidx.wear.watchface.complications.data.ShortTextComplicationData
 import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
-import java.util.Calendar
+import com.dessalines.habitmaker.R
+import com.dessalines.habitmaker.db.AppDB
+import com.dessalines.habitmaker.db.HabitRepository
+import com.dessalines.habitmaker.db.utils.HabitFrequency
+import com.dessalines.habitmaker.db.utils.isCompletedToday
+import com.dessalines.habitmaker.db.utils.toBool
+import com.dessalines.habitmaker.ui.MainActivity
 
 /**
  * Skeleton for complication data source that returns short text.
  */
 class MainComplicationService : SuspendingComplicationDataSourceService() {
-    override fun getPreviewData(type: ComplicationType): ComplicationData? {
-        if (type != ComplicationType.SHORT_TEXT) {
-            return null
+    private val database by lazy { AppDB.getDatabase(this) }
+    val habitRepository by lazy { HabitRepository(database.habitDao()) }
+
+    override fun getPreviewData(type: ComplicationType): ComplicationData? =
+        when (type) {
+            ComplicationType.SHORT_TEXT -> {
+                shortTextComplicationData(
+                    text = "4",
+                    contentDescription =
+                        getText(R.string.remaining_habits).toString(),
+                )
+            }
+
+            //            ComplicationType.LONG_TEXT -> TODO()
+//            ComplicationType.MONOCHROMATIC_IMAGE -> TODO()
+//            ComplicationType.SMALL_IMAGE -> TODO()
+            else -> {
+                null
+            }
         }
-        return createComplicationData("Mon", "Monday")
+
+    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData {
+        // Filter the habits by today
+        val habits = habitRepository.getAllSync.filter { HabitFrequency.entries[it.frequency] == HabitFrequency.Daily }
+
+        // Check the completed count
+        val completed = habits.count { isCompletedToday(it.lastCompletedTime) }
+
+        // Don't count archived in the total for progress
+        val total = habits.filter { !it.archived.toBool() }.size
+
+        val remainingHabits = total - completed
+
+        return shortTextComplicationData(
+            text = remainingHabits.toString(),
+            contentDescription =
+                getText(R.string.remaining_habits).toString(),
+        )
     }
 
-    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData =
-        when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
-            Calendar.SUNDAY -> createComplicationData("Sun", "Sunday")
-            Calendar.MONDAY -> createComplicationData("Mon", "Monday")
-            Calendar.TUESDAY -> createComplicationData("Tue", "Tuesday")
-            Calendar.WEDNESDAY -> createComplicationData("Wed", "Wednesday")
-            Calendar.THURSDAY -> createComplicationData("Thu", "Thursday")
-            Calendar.FRIDAY -> createComplicationData("Fri", "Friday")
-            Calendar.SATURDAY -> createComplicationData("Sat", "Saturday")
-            else -> throw IllegalArgumentException("too many days")
-        }
-
-    private fun createComplicationData(
+    private fun shortTextComplicationData(
         text: String,
         contentDescription: String,
-    ) = ShortTextComplicationData
-        .Builder(
-            text = PlainComplicationText.Builder(text).build(),
-            contentDescription = PlainComplicationText.Builder(contentDescription).build(),
-        ).build()
+    ): ShortTextComplicationData {
+        val tapIntent =
+            PendingIntent.getActivity(
+                applicationContext,
+                0,
+                Intent(this, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                },
+                PendingIntent.FLAG_IMMUTABLE,
+            )
+
+        return ShortTextComplicationData
+            .Builder(
+                text = PlainComplicationText.Builder(text).build(),
+                contentDescription = PlainComplicationText.Builder(contentDescription).build(),
+            ).setTapAction(tapIntent)
+            .build()
+    }
 }
