95d9a85b0a10e8ea79d7948efb231c00f0c2605e
Dessalines <tyhou13@gmx.com>
2026-04-19 21:23:48 -0400

    Material v3 upgrade for WearOS

--- wearable/src/main/java/com/dessalines/habitmaker/ui/theme/Theme.kt
+++ wearable/src/main/java/com/dessalines/habitmaker/ui/theme/Theme.kt
@@ -1,11 +1,15 @@
 package com.dessalines.habitmaker.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.material3.dynamicColorScheme
 
 @Composable
 fun Theme(content: @Composable () -> Unit) {
+    val dynamicColorScheme = dynamicColorScheme(LocalContext.current)
     MaterialTheme(
         content = content,
+        colorScheme = dynamicColorScheme ?: MaterialTheme.colorScheme,
     )
 }
