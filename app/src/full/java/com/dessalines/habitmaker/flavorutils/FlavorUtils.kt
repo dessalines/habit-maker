625ea933e8c3dc8eaea1df7a1d7d7a2f171db133
Dessalines <tyhou13@gmx.com>
2026-04-30 20:09:43 -0400

    Remove flavorutils

--- app/src/full/java/com/dessalines/habitmaker/flavorutils/FlavorUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.dessalines.habitmaker.flavorutils
-
-import android.util.Log
-import com.dessalines.habitmaker.utils.TAG
-import com.google.android.gms.common.GoogleApiAvailability
-import com.google.android.gms.common.api.AvailabilityException
-import com.google.android.gms.common.api.GoogleApi
-import kotlinx.coroutines.tasks.await
